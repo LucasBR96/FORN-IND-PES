eb3af7b75073b70927e2aa268abc2d76f8d137fc
LucasBR96 <lucasfuzatocipriano@hotmail.com>
2021-09-04 14:36:35 -0300

    ultimo commit antes da exclusao

--- trab6/settings.py
+++ trab6/settings.py
@@ -11,9 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -57,7 +58,7 @@ ROOT_URLCONF = 'trab6.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -79,7 +80,7 @@ WSGI_APPLICATION = 'trab6.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
---
+++ trab6/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from categoria.models import Categoria
+from produto.models import Produto
+
+def base( request ):
+
+    render_items = {}
+
+    nomes = []
+    for cat in Categoria.objects.all( ).order_by('nome'):
+        nomes.append( cat.nome )
+    render_items[ 'catg_list' ] = nomes
+
+    prods = []
+    for prod in Produto.objects.all( ).order_by('nome'):
+        prods.append( prod.get_render_tup() )
+    render_items[ 'prod_list' ] = prods
+
+    return render(request, 'base.html' , render_items )
